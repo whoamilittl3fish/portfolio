26a505a1c5e1f6a38738f60b402dfc37c9767083
Viet Khoa <khoa.ngovoviet@gmail.com>
2025-10-28 23:33:56 +0700

    fix: reverse the toggle btn context

--- js/theme.js
+++ js/theme.js
@@ -3,22 +3,21 @@ const body = document.body;
 
 const savedTheme = localStorage.getItem("theme");
 
-if (savedTheme === null || savedTheme === "dark") {
-  body.classList.add("dark-mode");
-  toggleBtn.textContent = "☀️ Light Mode";
+if (savedTheme === "dark"){
+    body.classList.add("dark-mode");
+    toggleBtn.textContent = "🌙 Dark Mode";
 } else {
-  body.classList.remove("dark-mode");
-  toggleBtn.textContent = "🌙 Dark Mode";
+    toggleBtn.textContent = "☀️ Light Mode";
 }
 
 toggleBtn.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
-    toggleBtn.textContent = "☀️ Light Mode";
+    toggleBtn.textContent = "🌙 Dark Mode";
     localStorage.setItem("theme", "dark");
   } else {
-    toggleBtn.textContent = "🌙 Dark Mode";
+    toggleBtn.textContent = "☀️ Light Mode";
     localStorage.setItem("theme", "light");
   }
 
