2cc461d068125d7543c49074f850de8522846e8d
khoa <khoa.ngovoviet@gmail.com>
2026-03-28 16:15:36 +0700

    feat: update SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://zoskisk.vercel.app',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date(),
+  })],
   markdown: {
     shikiConfig: {
       themes: {
